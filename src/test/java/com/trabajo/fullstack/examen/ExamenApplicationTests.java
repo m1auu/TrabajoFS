c38c1a850957b82c288d5dc4a88775b4ed5466c6
MireiaParra <miahigurashi02@gmail.com>
2023-11-08 14:36:08 +0100

    Inicio proyecto.

---
+++ src/test/java/com/trabajo/fullstack/examen/ExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.fullstack.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
